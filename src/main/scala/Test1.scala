42fef747d4c94ab5b99fa17edc20c43bce8bc2f4
aappddeevv <aappddeevv@gmail.com>
2018-12-22 16:53:41 -0500

    Test updates

--- src/main/scala/Test1.scala
+++ src/main/scala/Test1.scala
@@ -35,6 +35,4 @@ object Test1 {
     (s, respopt) => IO.fail(ClientBackendError(s)),
     answers.get
   )
-
-
 }
--- src/test/scala/Test1Spec.scala
+++ src/test/scala/Test1Spec.scala
@@ -56,5 +56,29 @@ class Test1Spec
     ioa.expectFailure(ExitResult.Cause.checked(ClientBackendError(Backend.message)))
   }
 
+  it should "3: be clear that an effect does not have a hidden failure in the effect" in {
+    val ioa: IO[ClientErrorBase, Assertion] =
+      backendClient3.expectOr[String](notInAnswers, decoders.passthrough _)(
+        (s, resp) => IO.fail(ClientError(s))
+      ).flatMap(bodystr => IO.now(bodystr shouldBe ""))
+
+    // Let's assume we catch all errors now...and we don't want to guess whether
+    // there is a Throwable in there but not known. This mimics the final layer
+    // in a UI. Normally, we would map the result into a UI presentable value or
+    // a user level error message on the left.
+    val ioa2: IO[Option[String], Assertion] = ioa.catchAll {
+      case ClientBackendError(message) => IO.fail(Option(message))
+      case ClientError(message) => IO.fail(Option(message))
+    }
+    // Normally we would dhave add an IO[Assertion] and would not know if errors
+    // had been handled or not. Even if it was IO[Either[E, Assertion]] the IO
+    // could still have an error and we would have to check.
+ 
+    // if E or A update the UI as needed
+    // ...
+
+    ioa2.expectFailure
+  }
+
 
 }
